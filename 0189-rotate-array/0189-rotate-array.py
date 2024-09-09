aacec26b014710a970161f650d8bed41c0f7dbf6
Arslan Ali <105268642+arslanali19470@users.noreply.github.com>
2024-09-10 01:06:17 +0500

    Time: 144 ms (85.38%), Space: 23 MB (86.4%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,6 +1,8 @@
 class Solution(object):
     def rotate(self, nums, k):
         k = k % len(nums)
+        listOne=nums[-k:]
+        listTwo=nums[:-k]
         nums[:]=nums[-k:]+nums[:-k]
       
  
