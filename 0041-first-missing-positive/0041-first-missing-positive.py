2fc96f8e0bfdee82ede06df8b67216d63b586a77
Arslan Ali <105268642+arslanali19470@users.noreply.github.com>
2024-09-17 07:14:41 +0500

    Time: 782 ms (5.26%), Space: 22.4 MB (87.17%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,22 +1,12 @@
 class Solution(object):
     def firstMissingPositive(self, nums):
-        Numone=False
-        n=len(nums)
-        for ind in range(n):
-            if nums[ind]==1:
-                Numone=True
-            if nums[ind]<1 or n<nums[ind]:
-                nums[ind]=1
-        if not Numone:
-            return 1
+        res=1
+        nums.sort()
+        for n in nums:
+            if n==res:
+                res+=1
+        return res
 
-        for ind in range(n):
-            index=abs(nums[ind])
-            nums[index-1]=-abs(nums[index-1])
-        for ind in range(n):
-            if nums[ind]>0:
-                return ind+1
-        return n+1
 
 
  
