065a2cfa034549afe6bc589d60bc175a1774e845
Arslan Ali <105268642+arslanali19470@users.noreply.github.com>
2024-10-08 09:51:15 +0500

    Time: 0 ms (100%), Space: 7.5 MB (72.79%) - LeetHub

---
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int m=n;
+        int answer;
+        int mask=0;
+
+        if (n==0){
+            return 1;
+        }
+        
+        while(m!=0){
+            mask = mask << 1 | 1;
+            m=m>>1;
+        }
+        answer= (~n)& mask;
+        return answer;
+    }
+};
