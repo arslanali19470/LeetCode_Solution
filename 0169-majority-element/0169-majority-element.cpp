4b5962707a3be256241a1328fb6bd13841f6cc60
Arslan Ali <105268642+arslanali19470@users.noreply.github.com>
2024-12-28 16:58:33 +0500

    Time: 0 ms (100%), Space: 28.2 MB (38.2%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int freq=1;
-        int ans=nums[0];
+        // Moores Vooting Algo:
         int n=nums.size();
-        sort(nums.begin(),nums.end());
-
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
+        int ans=0, freq=0;
+        for(int i=0;i<n;i++){
+            if(freq==0){
+                ans=nums[i];
+            }
+            if(ans==nums[i]){
                 freq++;
             }else{
-                freq=1;
-                
-            }
-            if(freq>n/2){
-                ans= nums[i];
+                freq--;
             }
         }
         return ans;
-       
         
     }
 };
